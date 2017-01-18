883a86c8242110223393416c71f07630a8ed5c92
Yann Dupont <dupont-y@jaws.cluster>
2017-01-18 14:24:58 +0100

    git is better on +Atom+Cross variant.
    Others changes.

--- void-uptodate/musl/build.sh
+++ void-uptodate/musl/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build --build-arg https_proxy=$http_proxy --build-arg http_proxy=$http_proxy $* -t objoo/voidlinux-musl .
+docker build --build-arg HTTPS_PRPXY=$HTTPS_PROXY --build-arg HTTP_PROXY=$HTTP_PROXY --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy $* -t objoo/voidlinux-musl .
